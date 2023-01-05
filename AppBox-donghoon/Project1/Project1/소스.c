0f168ae604180f8317b98488d15a144266bc3acf
amsrntk <107196864+amsrntk@users.noreply.github.com>
2023-01-06 01:27:08 +0900

    미연시
    미연시

---
+++ AppBox-donghoon/Project1/Project1/소스.c
@@ -0,0 +1,467 @@
+#include <stdio.h>
+#include <Windows.h>
+int next();
+int main(void)
+{
+	char name;
+	int instru;
+	int dong;
+
+	printf("동훈이가 만들었지만 무슨 제목으로 할 지 몰라서 이따구로 적는 미연시");
+	next();
+
+	printf("??? : 잠깐만!!!!!!!");
+	next();
+
+	printf("멀리서 시끄럽게 소리를 지르는 여자아이가 주변에 있는 사람들의 시선을 신경쓰지 않은 채 나에게로 찾아온다.");
+	next();
+
+	printf("저 여자아이는 어릴 때부터 알던 소꿉친구이다.");
+	next();
+
+	printf("그냥 어렸을 때부터 알던 사이라 친해진 친구?");
+	next();
+
+	printf("매일 학교를 같이 가긴 했지만, 김윤찬이 고등학교로 들어와서 늦잠이 많아지는 바람에 기다려주기 힘들어졌다.");
+	next();
+	
+	printf("김윤찬 : 또 늦잠을 잤어 ㅠㅠ..");
+	next();
+
+	printf("김윤찬 : 그래도 오늘은 따라잡았네!");
+	next();
+
+	printf("박선우 : 그래, 이번엔 내가 기다렸으니까.");
+	next();
+
+	printf("박선우 : 그리고 너가 그런 식으로 달려오면 사람들이 어떤 생각을 하겠어.");
+	next();
+	
+	printf("김윤찬 : 알았어.");
+	next();
+	
+	printf("김윤찬 : 그래도 날 기다려준 건 사실이잖아?");
+	next();
+	
+	printf("김윤찬 : 너, 말과 다르게 되게 자상하다?");
+	next();
+	
+	printf("시간이 지날수록 우리와 같이 등굣길을 걸으면서 이야기를 나누는 학생들이 늘어난다");
+	next();
+	
+	printf("김윤찬 : 혹시 동아리는 정했어?");
+	next();
+	
+	printf("박선우 : 동아리?");
+	next();
+	
+	printf("김윤찬 : 작년에 말했잖아 내년까지 안정하면 안된다고...");
+	next();
+	
+	printf("박선우 : 관심도 없어서 알아보지도 않았는데?");
+	next();
+	
+	printf("김윤찬 : 헤에?? 거짓말!");
+	next();
+	
+	printf("나는 동아리에도 관심도 없고, 그냥 애니를 보거나 게임을 하면서 노는게 좋으니까...");
+	next();
+	
+	printf("김윤찬 : 지금도 행복한 건 알겠지만 나중에 사회생활을 못해서 평생 백수가 될 수도 있단 말야...");
+	next();
+	
+	printf("너무 극단적이다. 밴드부에 안들어오면 백수가 된다라는 이야기로 들리기도 하고...");
+	next();
+	
+	printf("김윤찬 : 무슨 소리인지 알지?");
+	next();
+	
+	printf("박선우 : 알았어, 알았어....");
+	next();
+	
+	printf("박선우 : 너가 원한다면 동아리정도는 알아볼게.");
+	next();
+
+	printf("박선우 :  장담은 못해.");
+	next();
+	
+	printf("김윤찬 : 최소한 노력은 해준다고 약속해 줄 수있어..?");
+	next();
+	
+	printf("박선우 : 알았어.");
+	next();
+
+	printf("이야기를 하고 나니 벌써 학교가 눈앞에 있었다");
+	next();
+
+	printf("김윤찬 : 난 먼저 가볼게!");
+	next();
+
+	printf("박선우 : 그래.");
+	next();
+
+	printf("가방을 고리에 묶고 엎드린다");
+	next();
+
+	printf("30분 남았기도 하고, 김윤찬이랑 노는 것도 힘들어...");
+	next();
+
+	printf("Loading."); 
+	delay700();
+
+	printf("Loading..");
+	delay700();
+
+	printf("Loading...");
+	delay700();
+
+	printf("Loading.");
+	delay700();
+
+	printf("Loading..");
+	delay700();
+
+	printf("Loading...");
+	delay700();
+
+	printf("정신을 차리고 보니 벌써 7교시가 끝나는 종소리가 울린다");
+	next();
+	
+	printf("어느때와 다름없이 짐을 싸고 집을 갈려던 찰나....");
+	next();
+	
+	printf("김윤찬 : 박선우!!!!!!!!!");
+	next();
+	
+	printf("박선우 : ...?");
+	next();
+	
+	printf("김윤찬 : 어디가?");
+	next();
+	
+	printf("박선우 : 집");
+	next();
+	
+	printf("김윤찬 : 동아리는? ");
+	next();
+	
+	printf("박선우 : 동아리.. 아직 안정했어...");
+	next();
+	
+	printf("김윤찬 : 따라와.");
+	next();
+	
+	printf("박선우 : ㅇ..으응...");
+	next();
+	
+	printf("김윤찬은 나의 손을 잡고 밴드부로 끌려갔다");
+	next();
+	
+	printf("여자 1 : 뭐야 동아리에 온다는 애가 남자였어?"); //여자 1은 츤데레 여자 2는 교회눈나 조민성 여자 3은 부장 리더쉽이 강함 
+	next();
+	
+	printf("김윤찬 : 이 친구는 박세현, 피아노를 담당하고 있어!");
+	next();
+	
+	printf("여자 2 : 안녕하세요, 밴드부에 오신 것을 환영해요 ㅎㅎ");
+	next();
+	
+	printf("김윤찬 : 이 친구는 조민성, 밴드부 부장이고 기타담당이야.");
+	next();
+	
+	printf("박선우 : ㅇ...안녕하세요");
+	next();
+
+	printf("나는 알다시피 베이스담당이야.");
+	next();
+	
+	printf("ㄷ...다 여자였어??");
+	next();
+	
+	printf("김윤찬 : 자~ 이제 오늘부로 들어올 박선우라고 해.");
+	next();	
+	
+	printf("박선우 : 나는 아직 들어온다고 안했는데?");
+	next();	
+	
+	printf("다른 밴드부원들이 빤히 바라본다");
+	next();	
+	
+	printf("김윤찬 : 사실은.. 우리 부원이 1명 부족해서 없어질 예정이였거든...");
+	next();	
+
+	printf("[ 들어간다 : 1 ] / [ 안들어간다 : 2 ]\n");
+	scanf("%d", &dong);
+	
+	if (dong == 1) {
+		printf("박선우 : ㅇ..알았어 들어올게 ");
+		next();
+	}
+	else if (dong == 2)
+	{
+		printf("BadEnd [ 4 / 5 ]");
+		Sleep(3000);
+		system("cls");
+		printf("저");
+		delay700();
+		printf("저기");
+		delay700();
+		printf("저기 혹");
+		delay700();
+		printf("저기 혹시");
+		delay1000();
+		printf("끔");
+		delay500();
+		printf("끔찍");
+		delay500();
+		printf("끔찍한");
+		delay500();
+		printf("끔찍한 시");
+		delay500();
+		printf("끔찍한 시간");
+		delay500();
+		printf("끔찍한 시간을");
+		delay500();
+		printf("끔찍한 시간을 보");
+		delay500();
+		printf("끔찍한 시간을 보내");
+		delay500();		
+		printf("끔찍한 시간을 보내고 ");
+		delay500();
+		printf("끔찍한 시간을 보내고 싶");
+		delay500();
+		printf("끔찍한 시간을 보내고 싶어");
+		delay500();
+		printf("끔찍한 시간을 보내고 싶어?");
+		delay500();
+		while (1)
+		{
+			printf("@@");
+		}
+
+	}
+	
+	printf("밴두부원들 : 다행이야... ");
+	next();	
+	
+	printf("박세현 : 그나저나 넌 무슨 악기를 다뤄?");
+	next();	
+	
+	printf("[ 기타 : 1 ] / [ 피아노 : 2 ] [ 베이스 : 3 ]\n * 이스터에그있음 *");
+	scanf("%d", &instru);
+	next();
+	int instru2;
+	instru2 = instru;
+	if (instru == 1)
+	{
+		printf("잘은 못하지만 기타를 취미로 하고있어.\n");
+		next();
+	}
+
+	else if (instru == 2)
+	{
+		printf("잘은 못하지만 피아노를 취미로 하고있어.\n");
+		next();
+	}
+
+	else if(instru == 3)
+	{
+		printf("잘은 못하지만 베이스를 취미로 하고있어.\n");
+		next();
+	}
+	else if(instru == 4)
+	{
+		printf("BadEnd [ 5 / 5 ]");
+		Sleep(3000);
+		while (1)
+		{
+			printf("Just Monika");
+		}
+	}
+
+	if (instru2 == 1) 
+	{
+		printf("조민성 : 제가 많이 가르쳐 드릴게요.");
+		next();
+		printf("박선우 : 응, 부족하지만 부탁할게.");
+		next();
+	}
+	
+	else if (instru2 == 2)
+	{
+		printf("박세현 : 뭐?? 너가 피아노를 친다고..?");
+		next();
+		printf("박세현 : 어려운거 있으면 말하던가 말던가..."); 
+		next();
+		printf("박선우 : ㄱ..고마워...");
+		next();
+		printf("김윤찬 : 이래 봬도 착한 친구야.");
+		next();
+	}
+	
+	else if (instru2 ==3)
+	{
+		printf("김윤찬 : 너가 베이스를 했었어??");
+		next();
+
+		printf("박선우 : 관심 좀 가져..");
+		next();
+
+		printf("김윤찬 : 모르는거 있으면 나한테 맡겨!");
+		next();
+
+	}
+
+	printf("조민성 : 일단 소개도 다 끝냈고 시간도 늦었으니까 이만 여기서 해산할까?");
+	next();
+
+	printf("밴드부원 : 그럴까??");
+	next();
+
+	printf("조민성 : 그럼 일단 여기서 해산!");
+	next();
+
+	printf("흠.. 일단 힘드니까 집가서 생각해보자.");
+	next();
+
+	printf("집에 도착하고나니 머리가 새하얘졌다");
+	next();
+	if (instru == 1)
+	{
+		printf("잠시 잠든 사이 밴드부원중 조민성이 토요일에 기타를 알려준다고 연락이 왔다.");
+		next();
+		printf("알겠다고 한 채로 토요일이 되길 기다리고 있었다...");
+		next();
+		printf("지루한 평일이 끝나고 토요일이 되었다. 나는 약속장소를 가기위해 근처 녹음실로 향했다");
+		next();
+		printf("하루종일 기타를 치면서 데이트 아닌 데이트를 하고 늦어서 해산 할려는 그때!");
+		next();
+		printf("조민성 : ㅈ...저기 혹시..");
+		next();
+		printf("조민성 : 여자친구 있어?");
+		next();
+		printf("갑자기 대뜸 여자친구? 있을리가 없다..");
+		next();
+		printf("박선우 : 없어요.");
+		next();
+		printf("조민성 : 그.. 이런말 하기 그런데... 나랑 사귀어줄래?");
+		next();
+		printf("음?? 이게 무슨 급전개란 말인가..");
+		next();
+		printf("박선우 : 이유를 들어봐도 될까요?");
+		next();
+		printf("조민성 : 기타치는 것도 귀엽고... 처음 만났을 때부터 호감이 생겼어.");
+		next();
+		printf("사실 거절 할 이유는 없었다.");
+		next();
+		printf("나도 오늘 데이트 아닌 데이트를 하면서 호감이 생기기도 했고");
+		next();
+		printf("박선우 : 그럼.. 뭐 사귀죠.");
+		next();
+		printf("조민성 : 정말로? ㄱ..고마워");
+		next();
+		printf("오늘은 절대 잊지 못할거야.\n");
+		next();
+		printf("유달리 무덥고 매미가 사납게 울던 그 날은 여름이였다.\n");
+		printf("HappyEnd [ 3 / 5 ]");
+		
+	}
+	if (instru == 2)
+	{
+		printf("잠시 잠든 사이 밴드부원중 박세현이 토요일에 기타를 알려준다고 연락이 왔다.");
+		next();
+		printf("알겠다고 한 채로 토요일이 되길 기다리고 있었다...");
+		next();
+		printf("지루한 평일이 끝나고 토요일이 되었다. 나는 약속장소를 가기위해 근처 녹음실로 향했다");
+		next();
+		printf("하루종일 기타를 치면서 데이트 아닌 데이트를 하고 늦어서 해산 할려는 그때!");
+		next();
+		printf("박세현 : 야!");
+		next();
+		printf("박세현 : 너 혹시 여자친구 있어?");
+		next();
+		printf("갑자기 대뜸 여자친구? 있을리가 없다..");
+		next();
+		printf("박선우 : 없어요.");
+		next();
+		printf("박세현 : ㅇ..있잖아... 나랑 사귀자!");
+		next();
+		printf("음?? 이게 무슨 급전개란 말인가..");
+		next();
+		printf("박선우 : 이유를 들어봐도 될까요?");
+		next();
+		printf("박세현 : 그냥 사귀자고! 이유는 없어");
+		next();
+		printf("사실 거절 할 이유는 없었다.");
+		next();
+		printf("나도 오늘 데이트 아닌 데이트를 하면서 호감이 생기기도 했고");
+		next();
+		printf("박선우 : 그럼.. 뭐 사귀죠.");
+		next();
+		printf("박세현 : 진짜? 오늘부터 애인인거지?");
+		next();
+		printf("박선우 : 네.");
+		next();
+		printf("유달리 무덥고 매미가 사납게 울던 그 날은 여름이였다.\n");
+		printf("HappyEnd [ 1 / 5 ]");
+
+	}
+	if (instru == 3)
+	{
+		printf("잠시 잠든 사이 밴드부원중 김윤찬이 토요일에 기타를 알려준다고 연락이 왔다.");
+		next();
+		printf("알겠다고 한 채로 토요일이 되길 기다리고 있었다...");
+		next();
+		printf("지루한 평일이 끝나고 토요일이 되었다. 나는 약속장소를 가기위해 근처 녹음실로 향했다");
+		next();
+		printf("하루종일 기타를 치면서 데이트 아닌 데이트를 하고 늦어서 해산 할려는 그때!");
+		next();
+		printf("김윤찬 : 여자친구 없지?");
+		next();
+		printf("사실이지만 기분이 살짝 나빴다.");
+		next();
+		printf("박선우 : 없어.");
+		next();
+		printf("김윤찬 : 그럼 나랑 사귀자");
+		next();
+		printf("음?? 이게 무슨 급전개란 말인가..");
+		next();
+		printf("박선우 : 이유를 들어봐도 될까?");
+		next();
+		printf("조민성 : 너 내가 어렸을 때도 많이 어필했는데 알아차리지 못해서...");
+		next();
+		printf("사실 거절 할 이유는 없었다.");
+		next();
+		printf("나도 오늘 데이트 아닌 데이트를 하면서 호감이 생기기도 했고");
+		next();
+		printf("박선우 : 그럼.. 뭐 사귀자.");
+		next();
+		printf("김윤찬 : 그래! 그럼 오늘부터 1일이야");
+		next();
+		printf("유달리 무덥고 매미가 사납게 울던 그 날은 여름이였다.\n");
+		printf("HappyEnd [ 2 / 5 ]");
+
+	}
+}
+int next()
+{
+	puts("\n");
+	puts("[ 다음으로 가기 : Enter ]");
+	getchar();
+	system("cls");
+}
+int delay700()
+{
+	Sleep(700);
+	system("cls");
+}
+int delay1000()
+{
+	Sleep(1000);
+	system("cls");
+}
+int delay500()
+{
+	Sleep(500);
+	system("cls");
+}
